b5731a524784628830fc8ca3a03a59401836a644
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2019-06-14 15:30:21 +0200

    my notes with css

--- myNotes.js
+++ myNotes.js
@@ -1,6 +1,7 @@
 function main(){
   makeNoteForm();
   makeSeeListBtn();
+  makeSpaceForTable();
   const seeListBtn = document.getElementById("seeListBtn");
   seeListBtn.addEventListener("click", showMyNotes);
   
@@ -10,112 +11,125 @@ function main(){
             saveNote();
         }
     })
-
-    
 }
 
 function makeNoteForm(){
     const newForm = document.createElement("textarea");
     const title = document.createElement("input");
-    const breakPoint = document.createElement("br");
-
+   
     title.setAttribute("id", "title");
     title.setAttribute("placeholder", "title");
     newForm.setAttribute("placeholder", "write something");
     newForm.setAttribute("id", "content")
-   
 
-    // let button = document.createElement("button");
-    // button.addEventListener("click", saveNote);
-    // button.textContent = "submit";
- 
     document.querySelector(".container").appendChild(title);
-    document.querySelector(".container").appendChild(breakPoint);
     document.querySelector(".container").appendChild(newForm);
-    //document.querySelector(".container").appendChild(breakPoint);
-    //document.querySelector(".container").appendChild(button);
-   // document.querySelector(".container").appendChild(breakPoint);
-  
 }
+
 
 function makeSeeListBtn(){
     let seeListBtn = document.createElement("button");
     seeListBtn.setAttribute("id", "seeListBtn");
-    seeListBtn.textContent = "Show my notes";
+    seeListBtn.textContent = "myNotes";
     document.querySelector(".container").appendChild(seeListBtn);
 }
 
-function showMyNotes(){
-    drawTable();
-    fillWithNotes();
+function makeSpaceForTable(){
+    const spaceForTable = document.createElement("table");
+    spaceForTable.setAttribute("id", "tableSpace");
+    document.querySelector(".container").appendChild(spaceForTable);
+}
+
+const drawNewTable = function(title, content){
+    return `
+        <tr>
+            <td>${title}</td>
+            <td>${content}</td>
+        </tr>
+    `;
+};
+
+
+
+function addListener(){
+    const rows = document.getElementsByTagName("tr");
+    for(let i=0; i<rows.length; i++){
+        rows[i].addEventListener("click", function(){
+            edit(this);
+        })
+    }
+}
+
+function showMyNotes(){    
+    for (let i=0; i<localStorage.length; i++){
+        let keyName = localStorage.key(i);
+        
+        let content = localStorage.getItem(keyName);
+        let title = localStorage.key(i);
+        let tableElement = drawNewTable(title, content);
+        
+        document.querySelector("table").innerHTML += tableElement;
+    }
+    addListener();
 }
 
 function edit(elem){
-    console.log("edit");
-    const udpateForm = document.createElement("textarea");
-    udpateForm.setAttribute("id", "updateBtn");
-    udpateForm.textContent = elem.textContent;
-    document.querySelector(".container").appendChild(udpateForm);
+    const cells = elem.getElementsByTagName("td");
+    let title = cells[0];
+    let content = cells[1];
+
+    console.log(title); 
+    console.log(content);
     
-    const updateform = document.getElementsByTagName("textarea")[1];
-    updateform.addEventListener("keyup", function(event) {
+    const noteUpdForm = document.createElement("div");
+    noteUpdForm.classList.add("noteUpdForm");
+    const udpateContent = document.createElement("textarea");
+    udpateContent.setAttribute("id", "updateContent");
+    udpateContent.textContent = content.textContent;
+    
+    const udpateTitle = document.createElement("textarea");
+    udpateTitle.setAttribute("id", "updateTitle");
+    udpateTitle.classList.add("title");
+    udpateTitle.textContent = title.textContent;
+
+    document.querySelector(".container").appendChild(noteUpdForm);
+    document.querySelector(".noteUpdForm").appendChild(udpateTitle);
+    document.querySelector(".noteUpdForm").appendChild(udpateContent);
+    
+    const updateContentForm = document.getElementsByTagName("textarea")[2];
+    const updateTitleForm = document.getElementsByTagName("textarea")[1];
+
+    updateContentForm.addEventListener("keyup", function(event) {
         if (event.keyCode === 13){
             saveUpdateNote();
         }
     })
+
+    updateTitleForm.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13){
+            saveUpdateNote();
+            
+        }
+    })
 }
+
 
 function saveUpdateNote(){
-    let noteContent = document.getElementsByTagName("textarea")[1].value;
-    let noteTitle = document.getElementById("title").value;
+    let noteContent = document.getElementsByTagName("textarea")[2].value;
+    let noteTitle = document.getElementsByTagName("textarea")[1].value;
+    
     localStorage.setItem(noteTitle, noteContent);
-    document.getElementById("updateBtn").value = "";
+    document.location.reload(true);
 }
 
-function drawTable(){
-    const tbl = document.createElement("table");
-    const tblBody = document.createElement("tbody");
-  
-    for (let j = 0; j <= localStorage.length; j++) {
-      let row = document.createElement("tr");
-      row.addEventListener("click", function(){
-          edit(this)})
-        
-    //   for (let i = 0; i < 1; i++) {
-    //     var cell = document.createElement("td");
-    //     row.appendChild(cell);
-        
-    //   }
-  
-      tblBody.appendChild(row);
-    }
-    tbl.appendChild(tblBody);
-    document.querySelector(".container").appendChild(tbl);
-}
-
-
-
-function fillWithNotes(){
-    const table = document.getElementsByTagName("table")[0];
-    for (let j = 0; j <= localStorage.length; j++){
-        let keyName = localStorage.key(j);
-        table.rows[j].innerHTML = localStorage.getItem(keyName);
-        //table.rows[j].cells.innerHTML = localStorage.key(keyName);
-        // for (let i=0; i<rows.cells.length; i++){
-        //     table.rows.cells[i].innerHTML = localStorage.getItem(keyName)
-        // }
-        
-    }
-
-}
 
 function saveNote(){
     let noteContent = document.getElementsByTagName("textarea")[0].value;
     let noteTitle = document.getElementById("title").value;
     localStorage.setItem(noteTitle, noteContent);
+    
     document.getElementById("title").value = "";
     document.getElementById("content").value = "";
-
 }
 
 main()
